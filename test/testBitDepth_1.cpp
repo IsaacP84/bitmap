d5a3fe521b09650e0baf98977c38d9a0c52aef65
IsaacP84 <45881136+IsaacP84@users.noreply.github.com>
2025-09-12 22:17:24 -0500

    added tests

--- test/testBitDepth_1.cpp
+++ test/testBitDepth_1.cpp
@@ -1,5 +1,52 @@
 #include "test.h"
+#include <fstream>
 
+TEST(MatchTest, 10x10)
+{
+    const std::filesystem::path output_dir = std::filesystem::current_path().append("assets");
+    // This code generates a copy of "test/assets/BitDepth_1_Test_10_10.bmp"
+    // If the test fails this won't match
+    {
+        BMP map(10, 10, 1);
+        map.AddColor(0, 0, 0);
+        map.AddColor(255, 255, 255);
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+            {
+                map.SetPixel(i, j, 1);
+            }
+
+        map.SetPixel(1, 0, 0);
+        map.ToFile(std::filesystem::path(output_dir).append("BitDepth_1_Test_10_10_Attempt.bmp"));
+    }
+
+    // open both files and compare byte by byte
+    std::cout << "Comparing files..." << std::endl;
+    std::ifstream f1(std::filesystem::path(output_dir).append("BitDepth_1_Test_10_10_Attempt.bmp"), std::ios::binary);
+    std::ifstream f2(std::filesystem::path(output_dir).append("BitDepth_1_Test_10_10.bmp"), std::ios::binary);
+
+    ASSERT_TRUE(f1.is_open());
+    ASSERT_TRUE(f2.is_open());
+
+    char byte1, byte2;
+    while (f1.get(byte1) && f2.get(byte2))
+    {
+        // std::cout << std::hex << (int)(unsigned char)byte1 << " " << std::hex << (int)(unsigned char)byte2 << std::dec << std::endl;
+        ASSERT_EQ(byte1, byte2);
+    }
+    ASSERT_FALSE(f2.get(byte1));
+
+    // Check if both files reached the end
+    ASSERT_TRUE(f1.eof() && f2.eof());
+    f1.close();
+    f2.close();
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 // TEST(HelloTest, BasicAssertions)
 // {
 //     std::filesystem::path output_path = ".\\assets\\maze";
@@ -26,7 +73,6 @@
 //     {
 //         std::cout << output_path << " already exists." << std::endl;
 //     }
-
 
 //     BMP map(10, 10, 1);
 //     for (int i = 0; i < 10; i++)
--- test/testBitDepth_24.cpp
+++ test/testBitDepth_24.cpp
@@ -1,16 +1,10 @@
 #include "test.h"
 
-// TEST(HelloTest, BasicAssertions)
-// {
-//     return;
-// }
-
 class BitmapTest : public testing::Test
 {
 protected:
     BitmapTest()
     {
-        
         // Check for assets folder
         // If it doesn't exist, create it
         // Attempt to create the directory
@@ -26,8 +20,8 @@ protected:
             }
         }
     }
-    // ~BitmapTest() override = default;
-    std::filesystem::path output_dir = std::filesystem::current_path().append("assets");
+    ~BitmapTest() override = default;
+    const std::filesystem::path output_dir = std::filesystem::current_path().append("assets");
     uint8_t fileIncrementor = 0;
 
     
@@ -46,14 +40,14 @@ TEST_F(BitmapTest, WriteToFileSmall)
     for (int i = 0; i < 10; i++)
         for (int j = 0; j < 10; j++)
         {
-            m0_.setPixel(i, j, Color(255, 255, 255));
+            m0_.SetPixel(i, j, Color(255, 255, 255));
         }
-    m0_.setPixel(1, 0, Color(0, 0, 0));
+    m0_.SetPixel(1, 0, Color(0, 0, 0));
 
     std::string file_name = "bitDepth_24_test";
     file_name += (fileIncrementor++);
     file_name += ".bmp";
-    m0_.toFile(output_dir.append(file_name));
+    m0_.ToFile(std::filesystem::path(output_dir).append(file_name));
 }
 
 TEST_F(BitmapTest, WriteToFileMedium)
@@ -61,14 +55,14 @@ TEST_F(BitmapTest, WriteToFileMedium)
     for (int i = 0; i < 100; i++)
         for (int j = 0; j < 100; j++)
         {
-            m1_.setPixel(i, j, Color(255, 255, 255));
+            m1_.SetPixel(i, j, Color(255, 255, 255));
         }
-    m1_.setPixel(1, 0, Color(0, 0, 0));
+    m1_.SetPixel(1, 0, Color(0, 0, 0));
 
     std::string file_name = "bitDepth_24_test";
     file_name += (fileIncrementor++);
     file_name += ".bmp";
-    m1_.toFile(output_dir.append(file_name));
+    m1_.ToFile(std::filesystem::path(output_dir).append(file_name));
 }
 
 TEST_F(BitmapTest, WriteToFileLarge)
@@ -76,12 +70,12 @@ TEST_F(BitmapTest, WriteToFileLarge)
     for (int i = 0; i < 1000; i++)
         for (int j = 0; j < 1000; j++)
         {
-            m2_.setPixel(i, j, Color(255, 255, 255));
+            m2_.SetPixel(i, j, Color(255, 255, 255));
         }
-    m2_.setPixel(1, 0, Color(0, 0, 0));
+    m2_.SetPixel(1, 0, Color(0, 0, 0));
 
     std::string file_name = "bitDepth_24_test";
     file_name += (fileIncrementor++);
     file_name += ".bmp";
-    m2_.toFile(output_dir.append(file_name));
+    m2_.ToFile(std::filesystem::path(output_dir).append(file_name));
 }
